b0e4ca456799432a4b0c3b941106dbb5a0f9df39
lliwIWant <liwil5486@gmail.com>
2025-04-15 11:02:01 +0900

    mopcorn navbar style

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Route, Routes} from 'react-router'
+import AppLayout from './layout/AppLayout'
+import Homepage from './pages/Hompage/Homepage'
+import MovieDetail from './pages/MovieDetail/MovieDetail'
+import Movies from './pages/Movies/Movies'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    /* 설계 계획
+      홈페이지
+      영화 전체 보여주는 페이지(서치) /movie ? q
+      영화 디테일 페이지 /movies/:id
+      추천 영화 /movies/:id/recommandation
+      리뷰 /movies/:id/reviews  
+    */
+
+
+    // nav bar form
+    <div className='App'>
+        <Routes>
+          <Route path="/" element={<AppLayout/>}>
+                        {/* 요청 주소 부모 요소를 사용하면 index */}
+              {/* Homepage */}
+              <Route index element={<Homepage/>}/>
+              {/* Movie */}
+              <Route path="movies" element={<Movies/>}>
+                <Route path =":id" element ={<MovieDetail/>}/>
+              <Route/>
+              
+            </Route>
+          </Route>
+
+
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Outlet} from 'react-router';
+
+const AppLayout = () => {
+  return (
+    <div>
+        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+        <Container fluid>
+            <Navbar.Brand href="#" className='logo-title' >Mopcorn</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
+                <Nav.Link href="/" className='clickText'>Home</Nav.Link>
+                <Nav.Link href="/movies"  className='clickText'>Movies</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                />
+                <Button variant="outline-warning">Search</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default AppLayout
